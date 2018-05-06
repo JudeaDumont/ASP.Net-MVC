aa0a15be1f4da748e2d53df796c2766462838620
JudeaDumont <dumontjudea2@gmail.com>
2018-05-06 02:39:29 -0600

    Interactive Background and small code clean ups

--- WebApplication2/App_Start/BundleConfig.cs
+++ WebApplication2/App_Start/BundleConfig.cs
@@ -9,7 +9,11 @@ namespace WebApplication2
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/TweenLite.js",
+                        "~/Scripts/TweenMax.js",
+                        "~/Scripts/animatedbackground.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
